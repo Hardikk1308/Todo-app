8b546d6d8eef28078d02280ced9be8713e97f17d
hardik pawar <hardikpawar2811@gmail.com>
2025-04-25 01:22:07 +0530

    new commits

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@
 const app = require('./Src/app'); // Check correct folder name
 const { connectDB } = require('./config/db');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 connectDB(); // Connect to MongoDB
 
